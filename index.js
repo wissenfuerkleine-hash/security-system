d4fea65ae3a48b06f8a2d000a126e69093bdb3b3
wissenfuerkleine-hash <wissenfuerkleine@gmail.com>
2026-06-29 15:25:43 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const SecurityBot = require('./bot/bot');
-require('dotenv').config();
-
-const bot = new SecurityBot();
-bot.start();
-
-console.log('Discord Security System starting...');
-console.log('To unlock server: Set UNLOCK_SERVER=true in Railway Console');
